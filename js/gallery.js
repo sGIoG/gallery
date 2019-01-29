27608b8ee8c3d6bc6396bd91338cad9abbb7ff59
root <root@vps577239.ovh.net>
2019-01-29 21:57:17 +0100

    29012019_13

--- js/gallery.js
+++ js/gallery.js
@@ -133,7 +133,7 @@ class VerticalRenderer extends Renderer {
    */
   createPhotoElement(photo, width, config) {
     return this.createImageElement(photo,
-                                   px(width + (Math.random() * 10)),
+                                   px(width + (Math.random() * 150)),
                                    px(photo.height(width)),
                                    px(config.spacing));
   }
